ebe2cacd139108e20886f3c06cb78f3207156417
Sajad Rastegar <rastegar.sajjad@gmail.com>
2019-08-16 14:40:36 +0430

    Fix test errors and add some tests

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,11 +4,17 @@ class RepositoriesController < ApplicationController
     result = search_service.call
     @repos = result['items']
     @total_count = result['total_count']
+    render_failure unless @repos.any?
   end
 
   private
 
   def search_params
     params.require(:repositories).permit(:q)
+  end
+
+  def render_failure
+    render :index, status: 404
+    flash.now.alert = t('.failure')
   end
 end
--- test/controllers/repositories_controller_test.rb
+++ test/controllers/repositories_controller_test.rb
@@ -2,8 +2,34 @@ require 'test_helper'
 
 class RepositoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get repositories_index_url
+    mock = Minitest::Mock.new
+    mock.expect :body, {
+      items: ['some item'],
+      total_count: 1
+    }.to_json
+
+    mock.expect :is_a?, true, [Net::HTTPSuccess]
+
+    Net::HTTP.stub :get_response, mock do
+      get repositories_index_url(params: { repositories: { q: 'search key' }})
+    end
+
     assert_response :success
   end
 
+  test "should get failure when nothing found" do
+    mock = Minitest::Mock.new
+    mock.expect :body, {
+      items: [],
+      total_count: 1
+    }.to_json
+
+    mock.expect :is_a?, true, [Net::HTTPSuccess]
+
+    Net::HTTP.stub :get_response, mock do
+      get repositories_index_url(params: { repositories: { q: 'search key' }})
+    end
+
+    assert_response 404
+  end
 end
